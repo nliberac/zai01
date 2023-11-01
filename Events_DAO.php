538a5188cb99e2dd4f6ce10084d86db8b73597d9
Natalia <natalia.liberacka.priv@gmail.com>
2023-11-01 16:45:33 -0400

    zabezpieczenie polaczenia z baza danych

--- Events_DAO.php
+++ Events_DAO.php
@@ -13,7 +13,7 @@ class Events_DAO {
         $dbUser='db_admin';
         $dbPass='newsr00m!';
         try{
-            $this->pdo=new PDO("mysql:host=$dbHost;dbname=$dbName",$dbUser, $dbPass);
+            $this->pdo=new PDO("mysql:host=$dbHost;dbname=$dbName;charset=utf8mb4;sslmode=require",$dbUser, $dbPass);
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             
